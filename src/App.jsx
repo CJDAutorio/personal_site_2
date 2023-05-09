147310e535d74a7dc21cadad5b92330506a0c74b
CJDAutorio <cj.dautorio2021@gmail.com>
2023-05-08 20:39:27 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Landing from './pages/landing';
+import './css/App.css';
+
+function App() {
+
+  return (
+    <>
+      <Landing />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/particles.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { tsParticles } from "tsparticles-engine";
+
+
+function Landing() {
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+//   particlesJS.load('particles-js', 'assets/particles.json', function () {
+//     console.log('callback - particles.js config loaded');
+//   });
+
+  return (
+      <div id='tsparticles'></div>
+  )
+}
+
+export default Landing;
