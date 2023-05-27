4dff05c7a36621d8bd8798790595b41276816431
CJDAutorio <cj.dautorio2021@gmail.com>
2023-05-27 04:25:21 -0400

    Added PrevProjs component to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Particles from './js/pages/landing/components/particles';
 import Landing from './js/pages/landing/landing';
 import AboutMe from './js/pages/aboutMe/aboutMe';
 import WorkEdu from './js/pages/workEdu/workEdu';
+import PrevProjs from './js/pages/previousProjects/prevProjects';
 import Fade from 'react-reveal/Fade';
 import './css/App.css';
 
@@ -17,12 +18,15 @@ function App() {
       <div id='pages'>
         <Particles />
         <Landing />
-        <Fade bottom duration={1500} delay={0} opposite collapse>
+        <Fade bottom duration={1500} delay={0} opposite>
           <div>
             <AboutMe />
           </div>
           <div>
             <WorkEdu />
+          </div>
+          <div>
+            <PrevProjs />
           </div>
         </Fade>
       </div>
