6650ba3bb1a4372f657510a1724b8a7b07d1546f
CJDAutorio <cj.dautorio2021@gmail.com>
2023-05-27 04:26:49 -0400

    Create prevProjects.jsx

---
+++ src/js/pages/previousProjects/prevProjects.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import Fade from 'react-reveal/Fade';
+import ProjectCard from '../../components/cards';
+import PawTracksScreen from "../../../assets/img/pawtracks-screenshot.png";
+
+function PrevProjs() {
+
+  useEffect(() => {
+  });
+
+  return (
+    <div id='prev-proj' className='page'>
+      <div className='page-header'>
+        <h1 className='header'>PREVIOUS PROJECTS</h1>
+      </div>
+      <div className='page-content'>
+        <Fade bottom duration={1250} delay={0} opposite cascade>
+          <div className='flex-content-col'>
+            <div className='subsection'>
+              <div className='flex-content-row'>
+                <ProjectCard
+                  projectId='pawtracks'
+                  projectName='Senior Year Capstone - PawTracks'
+                  projectImage={PawTracksScreen}
+                  projectLink='https://github.com/g445g445/PawTracks'
+                  projectDesc={
+                    <p>An online petsitter powered by TensorFlow which allows users to monitor their pet through a webcam or a mobile device.</p>
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </div>
+    </div>
+
+  );
+};
+
+export default PrevProjs;
