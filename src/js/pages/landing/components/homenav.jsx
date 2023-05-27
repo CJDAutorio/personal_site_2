75f634141fe2364cb7671e46449a3ac34404c7a6
CJDAutorio <cj.dautorio2021@gmail.com>
2023-05-27 14:05:41 -0400

    Removed listeners, created onClick functions

--- src/js/pages/landing/components/homenav.jsx
+++ src/js/pages/landing/components/homenav.jsx
@@ -4,27 +4,13 @@ import Scroll from '../../../utils/scroll';
 function HomeNav() {
   const scroll = new Scroll();
 
-  useEffect(() => {
-    const aboutMeButton = document.getElementById('about-me-nav');
-    const workEduButton = document.getElementById('work-edu-nav');
-    const previousProjectsButton = document.getElementById('projects-nav');
-    const hobbiesInterestsButton = document.getElementById('hobbies-nav');
-    const contactMeButton = document.getElementById('contact-nav');
-
-    aboutMeButton.addEventListener('click', function () { scroll.scrollToElement('about-me') });
-    workEduButton.addEventListener('click', function () { scroll.scrollToElement('work-edu') });
-    previousProjectsButton.addEventListener('click', function () { scroll.scrollToElement('prev-proj') });
-    hobbiesInterestsButton.addEventListener('click', function () { scroll.scrollToElement('hobbies-interests') });
-    contactMeButton.addEventListener('click', function () { scroll.scrollToElement('contact-me') });
-  });
-
   return (
     <div id='home-navbar'>
-      <button className='home-nav-button' id='about-me-nav'>About Me</button>
-      <button className='home-nav-button' id='work-edu-nav'>Work/Edu</button>
-      <button className='home-nav-button' id='projects-nav'>Previous Projects</button>
-      <button className='home-nav-button' id='hobbies-nav'>Hobbies/Interests</button>
-      <button className='home-nav-button' id='contact-nav'>Contact Me</button>
+      <button className='home-nav-button' id='about-me-nav' onClick={() => scroll.scrollToElement('about-me')}>About Me</button>
+      <button className='home-nav-button' id='work-edu-nav' onClick={() => scroll.scrollToElement('work-edu')}>Work/Edu</button>
+      <button className='home-nav-button' id='projects-nav' onClick={() => scroll.scrollToElement('prev-proj')}>Previous Projects</button>
+      <button className='home-nav-button' id='hobbies-nav' onClick={() => scroll.scrollToElement('hobbies-interests')}>Hobbies/Interests</button>
+      <button className='home-nav-button' id='contact-nav' onClick={() => scroll.scrollToElement('contact-me')}>Contact Me</button>
     </div>
   )
 }
