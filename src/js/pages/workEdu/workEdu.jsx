2d32ba62af57621b02efa4b596bd072938086308
CJDAutorio <cj.dautorio2021@gmail.com>
2023-05-27 04:26:58 -0400

    Update workEdu.jsx

--- src/js/pages/workEdu/workEdu.jsx
+++ src/js/pages/workEdu/workEdu.jsx
@@ -15,43 +15,24 @@ function WorkEdu() {
         <Fade bottom duration={1250} delay={0} opposite cascade>
           <div className='flex-content-col'>
             <div className='subsection'>
-              <h2 className='subheader'>Education</h2>
+              <h2 className='subheader'>🎓Education</h2>
               <div className='entry'>
-                <h3 className='entry-title'>Solutions Architect Intern - Ping Identity - Denver, CO</h3>
-                <p>
-                  I worked as a Solutions Architect intern from June, 2022 to August, 2022 for Ping
-                  Identity in their office in Denver, Colorado. While at Ping, I was tasked with some
-                  of the following:
-                </p>
-                <ul>
-                  <li>
-                    Communicated with customers to help find the correct product or solution for the
-                    customer’s issues and use cases.
-                  </li>
-                  <li>
-                    Developed product demonstrations to assist Solutions Architects in communicating
-                    our products with customers using HTML, CSS, JS, and the DaVinci platform.
-                  </li>
-                  <li>
-                    Developed NodeJS plugins for Ping Identity’s DaVinci orchestration platform
-                    utilizing the Axios HTTP client and Postman libraries to create RESTful API calls
-                    to services.
-                  </li>
-                  <li>
-                    Studied Ping Identity products and their use cases to be able to communicate
-                    accurate and useful information to customers.
-                  </li>
-                </ul>
+                <h3 className='entry-title'>The University of North Carolina at Charlotte ⛏</h3>
+                <div className='edu-flex'>
+                  <p>Bachelor of Science in Computer Science</p>
+                  <p>Concentration in Web & Mobile App Dev</p>
+                  <p>Graduated in May 2023</p>
+                </div>
               </div>
             </div>
             <hr className='subsection-break'/>
             <div className='subsection'>
-              <h2 className='subheader'>Work History</h2>
+              <h2 className='subheader'>💻Work History</h2>
               <div className='entry'>
                 <h3 className='entry-title'>Solutions Architect Intern - Ping Identity - Denver, CO</h3>
                 <p>
                   I worked as a Solutions Architect intern from June, 2022 to August, 2022 for Ping
-                  Identity in their office in Denver, Colorado. While at Ping, I was tasked with some
+                  Identity in their office in Denver, Colorado.<br />While at Ping, I was tasked with some
                   of the following:
                 </p>
                 <ul>
@@ -79,7 +60,7 @@ function WorkEdu() {
                 <h3 className='entry-title'>Deli Clerk - Lowes Foods - Mooresville, NC</h3>
                 <p>
                   I also worked as a deli clerk in the Lowes Foods grocery store from May 2020 to
-                  September 2020
+                  September 2020.
                 </p>
                 <ul>
                   <li>
