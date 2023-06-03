2b5ecc3a6f6891c111eb56ed591b0396f2750b09
CJDAutorio <cj.dautorio2021@gmail.com>
2023-06-03 18:18:04 -0400

    Create hobbiesInterests.jsx

---
+++ src/js/pages/hobbiesInterests/hobbiesInterests.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import Fade from 'react-reveal/Fade';
+import Nav from '../../components/nav';
+import ProjectCard from '../../components/cards';
+import PawTracksScreen from "../../../assets/img/pawtracks-screenshot.png";
+
+function HobbiesInterests() {
+
+  useEffect(() => {
+  });
+
+  return (
+    <div id='hobbies-interests' className='page'>
+      <div className='page-header'>
+        <h1 className='header'>HOBBIES AND INTERESTS</h1>
+        <Nav />
+      </div>
+      <div className='page-content'>
+        <Fade bottom duration={1250} delay={0} opposite cascade>
+          <div className='flex-content-col'>
+            <div className='subsection'>
+              <div className='flex-content-row'>
+                <ProjectCard
+                  projectId='pawtracks'
+                  projectName='Senior Year Capstone - PawTracks'
+                  projectImage={PawTracksScreen}
+                  projectGitLink='https://github.com/g445g445/PawTracks'
+                  projectPageLink='https://stage.pawtracks.live/'
+                  projectDesc={
+                    <p>An online petsitter powered by TensorFlow which allows users to monitor their pet through a webcam or a mobile device.</p>
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </div>
+    </div>
+
+  );
+};
+
+export default HobbiesInterests;
