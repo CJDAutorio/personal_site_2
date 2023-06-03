a1cb8075b93a712820eef9e0ef7f70f58e897f9e
CJDAutorio <cj.dautorio2021@gmail.com>
2023-06-03 18:18:20 -0400

    Added links to project cards

--- src/js/pages/previousProjects/prevProjects.jsx
+++ src/js/pages/previousProjects/prevProjects.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Fade from 'react-reveal/Fade';
+import Nav from '../../components/nav';
 import ProjectCard from '../../components/cards';
 import PawTracksScreen from "../../../assets/img/pawtracks-screenshot.png";
 
@@ -12,6 +13,7 @@ function PrevProjs() {
     <div id='prev-proj' className='page'>
       <div className='page-header'>
         <h1 className='header'>PREVIOUS PROJECTS</h1>
+        <Nav />
       </div>
       <div className='page-content'>
         <Fade bottom duration={1250} delay={0} opposite cascade>
@@ -22,7 +24,8 @@ function PrevProjs() {
                   projectId='pawtracks'
                   projectName='Senior Year Capstone - PawTracks'
                   projectImage={PawTracksScreen}
-                  projectLink='https://github.com/g445g445/PawTracks'
+                  projectGitLink='https://github.com/g445g445/PawTracks'
+                  projectPageLink='https://stage.pawtracks.live/'
                   projectDesc={
                     <p>An online petsitter powered by TensorFlow which allows users to monitor their pet through a webcam or a mobile device.</p>
                   }
