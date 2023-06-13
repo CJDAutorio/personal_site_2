544406ca1d8df0667d65a6233ab78c6c7ca2c0b5
CJDAutorio <cj.dautorio2021@gmail.com>
2023-06-12 23:28:35 -0400

    Removed cardType

--- src/js/pages/hobbiesInterests/hobbiesInterests.jsx
+++ src/js/pages/hobbiesInterests/hobbiesInterests.jsx
@@ -5,11 +5,6 @@ import { BsCaretLeftFill, BsFillCaretRightFill } from 'react-icons/bs';
 import { LastFmCard } from '../../components/cards';
 
 function HobbiesInterests() {
-  const [lastFmCardType, setLastFmCardType] = useState(2);
-
-  const updateCardType = (newCardType) => {
-    setLastFmCardType(newCardType);
-  }
 
   return (
     <div id='hobbies-interests' className='page'>
@@ -21,16 +16,8 @@ function HobbiesInterests() {
         <div className='flex-content-col'>
           <div className='subsection'>
             <h2 className='subheader'>Music</h2>
-            <div className='lastfm-card-buttons'>
-              {lastFmCardType > 1 && (
-                <button className='lastfm-card-button-backward' onClick={() => { updateCardType(lastFmCardType - 1) }}><BsCaretLeftFill /></button>
-              )}
-              {lastFmCardType < 3 && (
-                <button className='lastfm-card-button-forward' onClick={() => { updateCardType(lastFmCardType + 1) }}><BsFillCaretRightFill /></button>
-              )}
-            </div>
             <LastFmCard
-              lastFmCardType={lastFmCardType}
+              lastFmCardType={2}
             />
           </div>
           <div className='subsection'>
