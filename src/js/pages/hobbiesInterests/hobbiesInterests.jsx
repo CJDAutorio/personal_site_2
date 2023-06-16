67a575121f57de1b448171ae39fad3ad5438162c
CJDAutorio <cj.dautorio2021@gmail.com>
2023-06-15 22:06:50 -0400

    Added generic cards

--- src/js/pages/hobbiesInterests/hobbiesInterests.jsx
+++ src/js/pages/hobbiesInterests/hobbiesInterests.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 import Fade from 'react-reveal/Fade';
 import Nav from '../../components/nav';
 import { BsCaretLeftFill, BsFillCaretRightFill } from 'react-icons/bs';
-import { LastFmCard } from '../../components/cards';
+import { LastFmCard, Card } from '../../components/cards';
+import FlagFootball from '../../../assets/video/flagfootball_crop.mp4';
+import Gym from '../../../assets/video/gym.mp4';
 
 function HobbiesInterests() {
 
@@ -22,6 +24,20 @@ function HobbiesInterests() {
           </div>
           <div className='subsection'>
             <h2 className='subheader'>Sports and Activities</h2>
+            <div id='hobby-cards'>
+              <Card
+                cardId='intramurals'
+                cardGraphic={FlagFootball}
+                cardName='Intramurals'
+                cardDesc={<div><p>Casual league 4v4 and 7v7 flag football demon.</p><br /><p>Shoutout Tom, Codie, Ben M, Ben P, Eric, and Evan.</p></div>}
+              />
+              <Card
+                cardId='gym'
+                cardGraphic={Gym}
+                cardName='Lifting'
+                cardDesc={<div><p><b>Bench:</b>&emsp;235 lbs</p><br /><p><b>Squat:</b>&emsp;1 slipped disc</p><br /><p><b>Deadlift:</b>&emsp;Gym didn't let me</p></div>}
+              />
+            </div>
           </div>
         </div>
       </div>
