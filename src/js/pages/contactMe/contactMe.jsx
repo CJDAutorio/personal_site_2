3c81d458eb81350a11032b607b54a80808cd4c75
CJDAutorio <cj.dautorio2021@gmail.com>
2023-06-17 17:48:56 -0400

    Switched from form to direct link (temp)

--- src/js/pages/contactMe/contactMe.jsx
+++ src/js/pages/contactMe/contactMe.jsx
@@ -6,8 +6,19 @@ import Nav from '../../components/nav';
 
 function ContactMe() {
 
-  useEffect(() => {
-  });
+  function emailLinkUnderline(direction) {
+    let underline;
+    if (document.getElementById('contact-email-underline-color')) {
+      underline = document.getElementById('contact-email-underline-color');
+      if (direction > 0) {
+        underline.style.width = '100%';
+        underline.style.borderColor = 'rgba(var(--primary-color-numbers), 1)';
+      } else {
+        underline.style.width = '0';
+        underline.style.borderColor = 'rgba(var(--primary-color-numbers), 0)';
+      }
+    }
+  }
 
   return (
     <div id='contact-me' className='page'>
@@ -23,9 +34,15 @@ function ContactMe() {
               <a className='contact-button' id='contact-linkedin' href='https://www.linkedin.com/in/cj-dautorio/' target='_blank' rel='noopener noreferrer'><BsLinkedin /></a>
               <p className='contact-subtitle'>@cj-dautorio</p>
             </div>
-            <div className='subsection'>
+            <div className='subsection' id='contact-email'>
               <h3>Or send me an email directly!</h3>
-              <EmailForm />
+              {/* <EmailForm /> */}
+              <a href='mailto: cjdautorioalt@gmail.com' id='contact-email-link' onMouseOver={() => {emailLinkUnderline(1)}} onMouseOut={() => {emailLinkUnderline(0)}}>
+                cjdautorioalt@gmail.com
+                <div id='contact-email-underline'>
+                  <div id='contact-email-underline-color'></div>
+                </div>
+              </a>
             </div>
           </div>
         </Fade>
