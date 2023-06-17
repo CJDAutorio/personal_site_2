06633a6bf224a22ffc1610b107a747c33f2970ff
CJDAutorio <cj.dautorio2021@gmail.com>
2023-06-17 03:45:40 -0400

    Separated form to separate component

--- src/js/pages/contactMe/contactMe.jsx
+++ src/js/pages/contactMe/contactMe.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
+import { BsLinkedin } from 'react-icons/bs';
 import Fade from 'react-reveal/Fade';
+import EmailForm from './components/emailForm';
 import Nav from '../../components/nav';
 
 function ContactMe() {
@@ -16,9 +18,14 @@ function ContactMe() {
       <div className='page-content'>
         <Fade bottom duration={1250} delay={0} opposite cascade>
           <div className='flex-content-col'>
+            <div className='subsection' id='contact-socials'>
+              <h3>Connect with me on <span className='highlight'>LinkedIn!</span></h3>
+              <a className='contact-button' id='contact-linkedin' href='https://www.linkedin.com/in/cj-dautorio/' target='_blank' rel='noopener noreferrer'><BsLinkedin /></a>
+              <p className='contact-subtitle'>@cj-dautorio</p>
+            </div>
             <div className='subsection'>
-              <div className='flex-content-row'>
-              </div>
+              <h3>Or send me an email directly!</h3>
+              <EmailForm />
             </div>
           </div>
         </Fade>
